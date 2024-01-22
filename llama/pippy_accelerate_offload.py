f28e2878d620d4cc3f2b7f65549e75778ab49ca6
Marc Sun <marc@huggingface.co>
2024-01-22 23:16:38 +0100

    update

--- llama/pippy_accelerate_offload.py
+++ llama/pippy_accelerate_offload.py
@@ -1,4 +1,3 @@
-# Attempting to use `pippy` with `bert` and `accelerate`'s `infer_device_map`
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from accelerate import PartialState
@@ -6,16 +5,17 @@ from accelerate.inference import prepare_pippy
 import inspect
 state = PartialState()
 
-# Create a blank model
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", low_cpu_mem_usage=True)
+# sdpa implementation which is the default torch>2.1.2 fails with the tracing + attention mask kwarg
+# with attn_implementation="eager" mode, the forward is very slow for some reason
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", low_cpu_mem_usage=True, attn_implementation="sdpa")
 model.eval()
 
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 prompts = ("I would like to", 'I really like to', 'The weather is') # bs = 3
 tokenizer.pad_token = tokenizer.eos_token
-inputs = tokenizer(prompts, return_tensors="pt", padding=True)
-
-model = prepare_pippy(model, example_args=(inputs['input_ids'],))
+inputs = tokenizer(prompts, return_tensors="pt", padding=True).to(0)
+inputs = {"input_ids": inputs["input_ids"]}
+model = prepare_pippy(model, example_kwargs=inputs)
 
 if state.is_main_process:
     def get_number_of_params(model):
@@ -23,14 +23,11 @@ if state.is_main_process:
     for i, sm in enumerate(model.pippy_stage.pipe.split_gm.children()):
         print(f"Pipeline stage {i} {get_number_of_params(sm) // 10 ** 6}M params")
 
-inputs = inputs.to(0)
-
 # doesn't work yet 
 # output = model.generate(**inputs, max_new_tokens=1)
 
 with torch.no_grad():
-    output = model(inputs['input_ids'])
-
+    output = model(**inputs)
 if output is not None:
     next_token_logits = output[0][:, -1, :]
     next_token = torch.argmax(next_token_logits, dim=-1)
